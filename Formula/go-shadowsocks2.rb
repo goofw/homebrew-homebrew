6afa690672e4a5f32d9372924be4c7933ed2afd6
goofw <59505666+goofw@users.noreply.github.com>
2021-05-25 10:18:50 +0000

    update go-shadowsocks2 acdbac0

--- Formula/go-shadowsocks2.rb
+++ Formula/go-shadowsocks2.rb
@@ -1,9 +1,9 @@
 class GoShadowsocks2 < Formula
   desc "Modern Shadowsocks in Go"
   homepage "https://github.com/shadowsocks/go-shadowsocks2"
-  version ""
-  url ""
-  sha256 ""
+  version "acdbac0"
+  url "https://github.com/goofw/go-shadowsocks2/releases/download/acdbac0/go-shadowsocks2-macos-64.zip"
+  sha256 "fb11e704714fb0c6b82399c70f68e8a7f94e8861b047faff4b6c64f201d24795"
   license "Apache-2.0"
 
   def install
