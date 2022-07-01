2a6fc64bbfc9bacf618ca366312b8ef3cefc8f4d
goofw <59505666+goofw@users.noreply.github.com>
2022-07-01 14:41:41 +0000

    update naiveproxy v103.0.5060.53-3

--- Formula/naiveproxy.rb
+++ Formula/naiveproxy.rb
@@ -1,9 +1,9 @@
 class Naiveproxy < Formula
   desc "Make a fortune quietly"
   homepage "https://github.com/klzgrad/naiveproxy"
-  version "v103.0.5060.53-2"
-  url "https://github.com/klzgrad/naiveproxy/releases/download/v103.0.5060.53-2/naiveproxy-v103.0.5060.53-2-mac-x64.tar.xz"
-  sha256 "8bdab220798a96c2f5d1ed2050e9ac5101a341984170c686303727f0c1ae940d"
+  version "v103.0.5060.53-3"
+  url "https://github.com/klzgrad/naiveproxy/releases/download/v103.0.5060.53-3/naiveproxy-v103.0.5060.53-3-mac-x64.tar.xz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "BSD"
 
   def install
