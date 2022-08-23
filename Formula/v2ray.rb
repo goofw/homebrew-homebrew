31f81a4570ce5fce97a71586efb2363eb9d0a1a3
goofw <59505666+goofw@users.noreply.github.com>
2022-08-23 18:49:47 +0000

    update v2ray v5.0.7

--- Formula/v2ray.rb
+++ Formula/v2ray.rb
@@ -3,7 +3,7 @@ class V2ray < Formula
   homepage "https://v2fly.org/"
   version "v5.0.7"
   url "https://github.com/goofw/v2ray/releases/download/v5.0.7/v2ray-macos-64.zip"
-  sha256 "f19cd7d22575c8e1d5f6198671681903d92c4161fca4b36ff4a17993211ff6f9"
+  sha256 "44da50cb348c43568c57b1bded790d237ba9961ebf6eba44005fc2acda3a256e"
   license all_of: ["MIT", "CC-BY-SA-4.0"]
 
   def install
