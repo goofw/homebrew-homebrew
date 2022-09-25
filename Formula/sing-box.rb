a7f192f0f58a22060233d0973316e3aea9eef96f
goofw <59505666+goofw@users.noreply.github.com>
2022-09-25 20:36:35 +0800

    Update sing-box.rb

--- Formula/sing-box.rb
+++ Formula/sing-box.rb
@@ -4,7 +4,7 @@ class Singbox < Formula
   version "v1.0.4"
   url "https://github.com/SagerNet/sing-box/releases/download/v1.0.4/sing-box-1.0.4-darwin-amd64.tar.gz"
   sha256 "aed1f0608aa3ae9981b55739556c3f18ba85e37237339cd6e9b29fb5a3d8fe9c"
-  license "GPL-3.0-or-later	"
+  license "GPL-3.0-or-later"
 
   def install
     bin.install "sing-box"
