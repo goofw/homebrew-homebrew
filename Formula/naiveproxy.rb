5d98b4fa33cdab14603e36fda36ac5187e10242f
goofw <59505666+goofw@users.noreply.github.com>
2022-11-10 20:44:51 +0000

    update naiveproxy v107.0.5304.87-2

--- Formula/naiveproxy.rb
+++ Formula/naiveproxy.rb
@@ -1,9 +1,9 @@
 class Naiveproxy < Formula
   desc "Make a fortune quietly"
   homepage "https://github.com/klzgrad/naiveproxy"
-  version "v107.0.5304.87-1"
-  url "https://github.com/klzgrad/naiveproxy/releases/download/v107.0.5304.87-1/naiveproxy-v107.0.5304.87-1-mac-x64.tar.xz"
-  sha256 "a58c8d93b1f38f8076c3b6988301f0c67438d8ba5a6b25ed3859aca335f442cf"
+  version "v107.0.5304.87-2"
+  url "https://github.com/klzgrad/naiveproxy/releases/download/v107.0.5304.87-2/naiveproxy-v107.0.5304.87-2-mac-x64.tar.xz"
+  sha256 "43d7b54bf15ae00186730be11eba599c3c400191a6664da3b55f74184a634012"
   license "BSD"
 
   def install
