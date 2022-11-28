5683cee03f752eeb7f5a6d0e92105acc13d6b145
goofw <59505666+goofw@users.noreply.github.com>
2022-11-28 15:36:34 +0000

    update sing-box v1.1-rc1

--- Formula/sing-box.rb
+++ Formula/sing-box.rb
@@ -3,7 +3,7 @@ class SingBox < Formula
   homepage "https://github.com/SagerNet/sing-box"
   version "v1.1-rc1"
   url "https://github.com/goofw/sing-box/releases/download/v1.1-rc1/sing-box-darwin-amd64.tar.gz"
-  sha256 "6b7e8ad6af1e7fd5a785a61ae1f17c00001247ff962cc2249a9ed33355abf6b9"
+  sha256 "b54759f718880155717f3c4e5da992c6dea18b0469d0ddc23cfa7e1c81bfbe59"
   license "GPL-3.0-or-later"
 
   def install
