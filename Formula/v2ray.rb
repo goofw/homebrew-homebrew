55ffcf0bdbab8ef93f704a32e636a1d6ade2e35d
goofw <59505666+goofw@users.noreply.github.com>
2023-11-19 01:42:47 +0000

    update v2ray v5.11.0

--- Formula/v2ray.rb
+++ Formula/v2ray.rb
@@ -1,9 +1,9 @@
 class V2ray < Formula
   desc "Platform for building proxies to bypass network restrictions"
   homepage "https://v2fly.org/"
-  version "v5.10.1"
-  url "https://github.com/v2fly/v2ray-core/releases/download/v5.10.1/v2ray-macos-64.zip"
-  sha256 "4bd661b535c329921e7c63cfe140d32bbb7ca9cbb5a9d92889bef190e6724d1b"
+  version "v5.11.0"
+  url "https://github.com/v2fly/v2ray-core/releases/download/v5.11.0/v2ray-macos-64.zip"
+  sha256 "4c0576593c0989a5c7025667180c05b6578ccb8738f5d0f845c883a36c444c02"
   license all_of: ["MIT", "CC-BY-SA-4.0"]
 
   def install
