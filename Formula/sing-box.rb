f704f46c73ca1774d24f025ef4bfc665e444d0bf
goofw <59505666+goofw@users.noreply.github.com>
2024-01-16 12:51:50 +0000

    update sing-box v1.8.2

--- Formula/sing-box.rb
+++ Formula/sing-box.rb
@@ -1,9 +1,9 @@
 class SingBox < Formula
   desc "The universal proxy platform"
   homepage "https://github.com/SagerNet/sing-box"
-  version "v1.8.1"
-  url "https://github.com/goofw/app/releases/download/v1.8.1/sing-box-darwin-amd64.tar.gz"
-  sha256 "577c8dfb4e19c57039dca370c3f40d5eca0233cc1d217653787032f24bd67e24"
+  version "v1.8.2"
+  url "https://github.com/goofw/app/releases/download/v1.8.2/sing-box-darwin-amd64.tar.gz"
+  sha256 "f2c5eda0feb4196e2d28f82333c6b1da56c8fe08804a2ad1fac6b2b53b453048"
   license "GPL-3.0-or-later"
 
   def install
